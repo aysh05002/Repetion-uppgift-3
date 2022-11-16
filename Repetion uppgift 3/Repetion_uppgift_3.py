a002eda08af8654ad37010ac79599dde74405efb
Admin <Admin@TEB123008>
2022-11-16 10:22:09 +0100

    Add project files.

---
+++ Repetion uppgift 3/Repetion_uppgift_3.py
@@ -0,0 +1,18 @@
+def information():
+    print("\n\t\t\t\t\t\t----------------------")
+    print("\t\t\t\t\t\t| Rubrik: Uppgift 3  |")
+    print("\t\t\t\t\t\t|                    |")
+    print("\t\t\t\t\t\t| Namn: Ayman Shakra |")
+    print("\t\t\t\t\t\t|                    |")
+    print("\t\t\t\t\t\t| Datum: 2022/11/16  |")
+    print("\t\t\t\t\t\t----------------------\n")
+
+information()
+
+C=int(input("\t\t\t\t\t\t Grader i celsius: "))
+print("\n\t\t\t\t\t\t----------------------------")
+F=(9/5)*C+32
+print("\t\t\t\t\t\t Celsius \t Fahrenheit ")
+print("\t\t\t\t\t\t----------\t-----------")
+print("\t\t\t\t\t\t",C,"\t\t",F,"")
+print("\t\t\t\t\t\t----------------------------")
